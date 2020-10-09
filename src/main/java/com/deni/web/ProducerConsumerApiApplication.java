f02f2e8eebc2874ade0f27618235527867fd37fc
Deni Simon <denimallel@gmail.com>
2020-10-09 20:15:42 +0530

    Initial commit

---
+++ src/main/java/com/deni/web/ProducerConsumerApiApplication.java
@@ -0,0 +1,13 @@
+package com.deni.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerConsumerApiApplication.class, args);
+    }
+
+}
